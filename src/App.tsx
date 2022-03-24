dcb5255eaa179cd2cc3d2a55f4b48320abd67860
Julieta <audino.js@gmail.com>
2022-03-24 20:28:13 -0300

    created basic app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ALittleComponent from "./components/ALittleComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <ALittleComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ALittleComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ALittleComponent = () => (
+    <div>
+        <h1>I'm a little Component</h1>
+    </div>
+)
+
+export default ALittleComponent;
